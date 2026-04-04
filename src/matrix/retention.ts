a02b4804194c28e6164f348ea234e1fc123d98d2
AnotherLaughingMan <AnotherLaughingMan@users.noreply.github.com>
2026-04-04 13:49:24 -0500

    chore: remove all source files

--- src/matrix/retention.ts
+++
@@ -1,6 +0,0 @@
-export const MAX_MESSAGES_TOTAL = 3000;
-export const MAX_MESSAGES_PER_ROOM = 600;
-
-export function getRetentionSummaryLabel() {
-  return `Retention: ${MAX_MESSAGES_PER_ROOM}/room, ${MAX_MESSAGES_TOTAL} total`;
-}
